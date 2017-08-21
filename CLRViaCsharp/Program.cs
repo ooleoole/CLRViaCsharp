e6d7103bf1f2b5bb01368176fff0015750d5d196
Ola Popp <olapopp85@gmail.com>
2017-08-21 20:50:47 +0200

    Add project files.

---
+++ CLRViaCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLRViaCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
